db4358598ad4826000f02968a39bd4096b2346a2
abrarShariar <shariarabrar@gmail.com>
2016-08-28 10:45:12 +0600

    titanic survival count using subset

--- titanic_survive_count.R
+++ titanic_survive_count.R
@@ -45,15 +45,41 @@ third_class <- subset(data,Class == '3rd')
 survive_count <- nrow(subset(third_class,Survived == "Yes"))
 print(paste("Third-Class Survivors: ",survive_count))
 
-
 #get data of Crew
 crew_class <- subset(data,Class == 'Crew')
 #print(crew_class)
 survive_count <- nrow(subset(crew_class,Survived == 'Yes'))
 print(paste("Crew Class Survivors: ",survive_count))
 
-
 #THIS IS WEIRD (each class showing equal no. of survivours)
 
+#get child survivors
+child_survivor <- nrow(subset(data,Age=='Child' & Survived=='Yes'))
+print(paste("Child Survivors: ",child_survivor))
+
+#get adult survivors
+adult_survivor <- nrow(subset(data,Age=='Adult' & Survived=='Yes'))
+print(paste("Adult Survivor: ",adult_survivor))
+
+#get male survivors
+male_survivor <-nrow(subset(data,Sex=='Male' & Survived=='Yes'))
+print(paste("Male Survivor: ",male_survivor))
+
+#get female survivors
+female_survivor <- nrow(subset(data,Sex=='Female' & Survived=='Yes'))
+print(paste("Female Survivor: ",female_survivor))
+
+
+#get male first class survivors
+male_first_class <-nrow(subset(data,Sex=='Male' & Class=='1st' & Survived=='Yes'))
+print(paste("Male First-Class Survivors: ",male_first_class))
+
+#get female first class survivours
+female_first_class <- nrow(subset(data,Sex=='Female' & Class=='1st' & Survived=='Yes'))
+print(paste("Female First-Class Survivors: ",female_first_class))
+
+#get male crew survivor
+male_crew_survivor <- nrow(subset(data,Class=='Crew' & Sex=='Male' & Survived=='Yes')) 
+print(paste("Male Crew Survivor: ",male_crew_survivor))
 
 
