94f365f169abcb2f82c3de7d283bb4fd60c5abb6
abrarShariar <shariarabrar@gmail.com>
2016-08-30 22:40:49 +0600

    range()

---
+++ range.R
@@ -0,0 +1,18 @@
+# calculating Range and Co-efficient of range
+
+# class work example 
+data <- c(89,73,84,91,87,77,94)
+print(range(data))
+res <- max(data) - min(data)
+print(res)
+
+#using sample datasets of R
+data <- USArrests$Murder
+print(range(data))
+res <- max(data) - min(data)
+print(res)
+
+#co-efficient of range in R 
+
+
+
